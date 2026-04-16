fc99a989560c12352518f6c767fb0391959df333
friedrich <wquangu@gmail.com>
2026-04-16 23:30:03 +0700

    Demo Get method using gin framework

--- main.go
+++ main.go
@@ -1,37 +1,36 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/demo", demoHandler)
-	log.Println("Server starting from log...")
-	fmt.Println("Server from fmt...")
-	error := http.ListenAndServe(":8080", nil)
-	if error != nil {
-		log.Fatal("Error starting server:", error)
-	}
-}
-func demoHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	response := map[string]string{
-		"message": "Hello, this is a json response!",
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("x-Course", "Golang")
-
-	data, err := json.Marshal(response)
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-	w.Write(data)
-
+	r := gin.Default()
+	r.GET("/demo", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Hello, this is a json response!",
+		})
+	})
+	r.GET("/users", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"users": "List of users",
+		})
+	})
+	r.GET("/users/:user_id", func(ctx *gin.Context) {
+		user_id := ctx.Param("user_id")
+		ctx.JSON(200, gin.H{
+			"data":    "User details",
+			"user_id": user_id,
+		})
+	})
+	r.GET("/product/:product_name", func(ctx *gin.Context) {
+		product_name := ctx.Param("product_name")
+		price := ctx.Query("price")
+		ctx.JSON(200, gin.H{
+			"data":         "Product details",
+			"product_name": product_name,
+			"price":        price,
+		})
+	})
+	r.Run(":8080")
 }
