c419661146180825321d722ab7e6043651d9b3d2
friedrich <wquangu@gmail.com>
2026-05-27 14:01:49 +0700

    remove node_modules

---
+++ frontend/src/modules/auth/views/LoginView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+import { loginApi } from '../service/auth'
+const username = ref('')
+const password = ref('')
+
+const login = async () => {
+    try {
+        await loginApi(username.value, password.value)
+    } catch (error) {
+        console.error('Login failed:', error)
+    }
+}
+</script>
+
+
+<template>
+    <div>
+        <h1>Login</h1>
+        <form @submit.prevent="login">
+            <div>
+                <label for="username">Username:</label>
+                <input id="username" v-model="username" type="text" required />
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input id="password" v-model="password" type="password" required />
+            </div>
+            <button type="submit" >Login</button>
+        </form>
+    </div>
+</template>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,9 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../modules/HomeView.vue'
+import LoginView from '@/modules/auth/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    
     {
       path: '/',
       name: 'home',
@@ -15,7 +22,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('../modules/AboutView.vue'),
     },
   ],
 })
